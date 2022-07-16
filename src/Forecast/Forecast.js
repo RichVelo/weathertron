ec539262f3206b763d76c90ffe4f41e7a805678c
RichVelo <richrar@gmail.com>
2022-07-16 16:05:34 +0100

    images and fetch

--- src/Forecast/Forecast.js
+++ src/Forecast/Forecast.js
@@ -1,9 +1,22 @@
 import './forecast.css';
 
 const Forecast = () => {
-    return <div className="forecast">
-                <p>Here will be forecasts...</p>
-           </div>
-}
+
+    fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Great%20Missenden/next4days?unitGroup=metric&key=JRQGFHWMLJJEMRQJ9Z6GXQ8GC&contentType=json", {
+        "method": "GET",
+        "headers": {
+        }
+    }).then((response) => {
+            console.log(response)
+            return response.json()
+        }).then((response) => {
+
+        // displayWeatherData(response); //need to define the function here
+    })
+
+
+
+            return //returning the card to be displayed pulling everything together (format for readability)
+    }
 
 export default Forecast;
