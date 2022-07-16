f2e0e754b3f737ab9312d9b5409f0a07acdd9f3d
RichVelo <richrar@gmail.com>
2022-07-16 15:41:08 +0100

    structure changes

---
+++ src/Forecast/Forecast.js
@@ -0,0 +1,9 @@
+import './forecast.css';
+
+const Forecast = () => {
+    return <div className="forecast">
+                <p>Here will be forecasts...</p>
+           </div>
+}
+
+export default Forecast;
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,8 +1,10 @@
+import './header.css'
+
 const Header = () => {
     return (
-        <>
+        <div className="header">
             <h1>Weather Tron 3000</h1>
-        </>
+        </div>
     )
 }
 
